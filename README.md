c048faa040363c196372c5573068fd04c0955552
Akshay Raichur <akshay.raichur9@gmail.com>
2022-11-25 11:18:21 +0530

    feat: n-level-comments completed

---
+++ README.md
@@ -0,0 +1,12 @@
+## Javascript App Template
+
+This app was bootstraped with [new-javascript-app](https://www.npmjs.com/package/new-javascript-app)
+
+#### Get Started
+
+- Open `index.html` with any live server.
+
+#### Learn More
+
+- Package Details: [new-javascript-app](https://www.npmjs.com/package/new-javascript-app)
+- Author: [Linkedin](https://www.linkedin.com/in/akshay-raichur) , [GitHub](https://github.com/akshayraichur)
---
+++ scripts/index.js
@@ -0,0 +1,73 @@
+const commentContainer = document.querySelector('.comment');
+
+// TODO: add validation checks everywhere
+
+const addNewComment = (e) => {
+  // get the parent node
+  const parentNode = e.target.parentNode;
+  // get the input value from sibling input elm.
+  const inputValue = parentNode.querySelector('.comment-input').value;
+
+  // remove contents from parent node
+  parentNode.innerHTML = ``;
+
+  // add a template of comment and reply btn
+  const addCommentTemplate = document.querySelector('#add-new-comment');
+  parentNode.appendChild(addCommentTemplate.content.cloneNode(true));
+
+  const commentElm = parentNode.querySelector('.comment-content');
+  commentElm.textContent = inputValue;
+};
+
+const addReply = (e) => {
+  const parentNode = e.target.parentNode;
+
+  const nestedCommentTemplate = document.querySelector(
+    '#nested-comment-template'
+  );
+  const nestedCommentContainer = document.createElement('div');
+  nestedCommentContainer.classList.add('nested-comment');
+  nestedCommentContainer.appendChild(
+    nestedCommentTemplate.content.cloneNode(true)
+  );
+
+  parentNode.appendChild(nestedCommentContainer);
+};
+
+const addNewCommentSection = (e) => {
+  const newCommentSection = document.createElement('div');
+  newCommentSection.classList.add('comment-container');
+
+  const comment = document.createElement('div');
+  comment.classList.add('comment');
+
+  const commentSectionTemplate = document.querySelector(
+    '#nested-comment-template'
+  );
+  comment.appendChild(commentSectionTemplate.content.cloneNode(true));
+
+  newCommentSection.appendChild(comment);
+
+  // get the parent of parent node
+  const parentNode = e.target.parentNode.parentNode.parentNode;
+  parentNode.appendChild(newCommentSection);
+};
+
+const handleClickListener = (e) => {
+  if (
+    e.target.classList.contains('add-comment-btn') &&
+    e.target?.parentNode?.classList?.contains('comment')
+  ) {
+    addNewCommentSection(e);
+  }
+
+  if (e.target.classList.contains('add-comment-btn')) {
+    addNewComment(e);
+  }
+
+  if (e.target.classList.contains('nested-comment-btn')) {
+    addReply(e);
+  }
+};
+
+document.addEventListener('click', handleClickListener);
